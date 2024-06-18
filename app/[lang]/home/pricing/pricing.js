a7981b0a6fe87a6acfa024351bf70234d7d72d5e
kevthefoo <kevthefoo@gmail.com>
2024-06-19 00:47:57 +1000

    pricing table modified

--- app/[lang]/home/pricing/pricing.js
+++ app/[lang]/home/pricing/pricing.js
@@ -6,8 +6,8 @@ export default function Pricing({ dict }) {
             <section className='stripe_pricing' id="pricing">
                 <h1 className='pricing_table_title'>{dict.pricing}</h1>
                 <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-                <stripe-pricing-table pricing-table-id="prctbl_1PSxymBaMQOMc3iJ7zdrkcRv"
-                    publishable-key="pk_test_51OiLl6BaMQOMc3iJcCqhnFcMCIFrEs2Dr7BIhemWGt1NejBPFMy7qYdjmIh6lXuw0H1S1sIzDOSMncCXjRJrVwBt00cGjkEOSd">
+                <stripe-pricing-table pricing-table-id="prctbl_1PT3JiBaMQOMc3iJ11zaJfUa"
+                    publishable-key="pk_live_51OiLl6BaMQOMc3iJhrhaPmW7qjClRo43NUqsn3ZqCDHw57FKhh6V6lEzzEi9ru9VUEupJs9Ac3dpCoCrWM8gkOba00U6y4rem6">
                 </stripe-pricing-table>
             </section>
         )
@@ -16,8 +16,8 @@ export default function Pricing({ dict }) {
             <section className='stripe_pricing' id="pricing">
                 <h1 className='pricing_table_title'>{dict.pricing}</h1>
                 <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-                <stripe-pricing-table pricing-table-id="prctbl_1PRVGnBaMQOMc3iJAlmHno1g"
-                    publishable-key="pk_test_51OiLl6BaMQOMc3iJcCqhnFcMCIFrEs2Dr7BIhemWGt1NejBPFMy7qYdjmIh6lXuw0H1S1sIzDOSMncCXjRJrVwBt00cGjkEOSd">
+                <stripe-pricing-table pricing-table-id="prctbl_1PT3HlBaMQOMc3iJmCvFY0l2"
+                    publishable-key="pk_live_51OiLl6BaMQOMc3iJhrhaPmW7qjClRo43NUqsn3ZqCDHw57FKhh6V6lEzzEi9ru9VUEupJs9Ac3dpCoCrWM8gkOba00U6y4rem6">
                 </stripe-pricing-table>
             </section>
         )
